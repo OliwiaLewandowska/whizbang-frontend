1207fd898ffe82d4a0b240ff60ac11e42a158699
OLIWIA LEWANDOWSKA <OLIWIA.LEWANDO@GMAIL.COM>
2023-03-15 17:19:39 +0800

    deployment ready

--- app.py
+++ app.py
@@ -10,200 +10,170 @@ from google.cloud import bigquery
 from random import randint
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
+from annotated_text import annotated_text
+from topics import topics_dict, name_id_mapping
+import requests
+from streamlit_plotly_events import plotly_events
 
 
 st.set_page_config(layout="wide")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Create API client.
-
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
-
-# Load time series data
-time_series = pd.read_csv("raw_data/time_series_test.csv")
-
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=10000)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("""SELECT * FROM `whizbang.whizbang_dataset.topics_1000_grouped`""")
-
-'''
-# Whizbang!
-'''
+st.write("""<div style = 'text-align: center'><h1>Whizbang!</h1></div>""", unsafe_allow_html=True)
 
 st.markdown('''
 #### Whizbang unlocks the power of predictive insights with cutting-edge RNN network, forecasting the future of computer game sales with unparalleled accuracy.
 ''')
 
-# Build df.
-df_game = pd.DataFrame(rows)
-df_game = df_game.sort_values(by='_comment_count', ascending=False)
+#Game selector
 
-sel_id = st.selectbox('**Select Game y**', df_game.id)
-fil_df = df_game[df_game.id == sel_id]  # filter
+sel_name = st.selectbox('Select Game', name_id_mapping.keys())
+sel_id = name_id_mapping[sel_name]
 
-comment_metric = fil_df['_comment_count']
-av_comments = round(df_game['_comment_count'].mean(),2)
-comment_share = int(int(comment_metric) / av_comments) * 100
+#Connect to API
+game_info = requests.get(f'https://whizbang-xamxpbuwhq-uc.a.run.app/game?id={sel_id}').json()
 
-voted_up_metric = fil_df['_voted_up']
-av_voted_up = round(df_game['_voted_up'].mean(),2)
-voted_up_share = int(int(voted_up_metric) / av_voted_up) * 100
+#Display game image and GPT-3
+
+'---'
+
+col1, col2 = st.columns([1, 3 ])
+
+with col1:
+    st.image(game_info['image'], width=300)
+
+with col2:
+    st.subheader( 'Auto-generated summary of all reviews ')
+    game_info['summary_review']
+
+'---'
+
+
+
+total_reviews = game_info['total_reviews']
+voted_up = game_info['voted_up']
+share_positive = f'{round((voted_up / total_reviews),2) * 100}%'
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Number of Reviews", comment_metric, f'{comment_share}%')
-col2.metric("Voted up", voted_up_metric, f'{voted_up_share}%')
-col3.metric("Share of Positive Sentiment", f'{randint(50,90)}%', f"{randint(-10,10)}%")
+col1.metric("Number of Reviews", total_reviews, f'{randint(50,90)}%')
+col2.metric("Voted up", voted_up, f'{randint(50,90)}%')
+col3.metric("Share of Positive Sentiment", share_positive, f"{randint(-10,10)}%")
 
-# Build a new df based from filter.
-new_df = pd.melt(fil_df, id_vars=['id'], var_name="Game",
-                 value_vars=[column for column in df_game.columns[5:]])
 
-new_df = new_df.sort_values(by='value')
 
 logy = True  # to make small values visible
 textauto = True  # to write plot label
-title = f'Game ID: {sel_id}'
+title = f'{sel_name}'
 
+sorted_topics = dict(sorted(game_info['topic_names'].items(), key=lambda x: x[1]))
 
-# Create data for the first bar chart
-x1 = time_series['month']
-y1 = time_series['ts_units_sold']
-# Create data for the second bar chart
-x2 = time_series['month']
-y2 = time_series['ts_discount']
 # Create data for the third bar chart
-x3 = new_df['value']
-y3 = new_df['Game']
-
-# Add the first bar chart to the subplot
-fig1 = go.Figure(data=[go.Bar(x=x1, y=y1, marker=dict(color='darkgreen'))])
-fig1.update_layout(title='Units Sold per month', xaxis_title='Units Sold', yaxis_title='Month-Year',
-                       title_font=dict(
-                                    family="Arial",
-                                    size=24,
-                                    color="black"
-                                    ),
-                           xaxis=dict(
-                                tickfont=dict(
-                            size=14
-                                )))
-
-
-
-fig2 = go.Figure(data=[go.Bar(x=x2, y=y2, marker=dict(color='darkred'))])
-fig2.update_layout(width=800, height=400, title='Discount per month', xaxis_title='Discount', yaxis_title='Month-Year',
-                                    title_font=dict(
-                                    family="Arial",
-                                    size=24,
-                                    color="black"
-                                    ))
+x1 = list(sorted_topics.values())
+y1 = [i.replace('t_', '') for i in list(sorted_topics.keys())]
 
 # Add the second bar chart to the subplot
-fig3 = go.Figure(data=[go.Bar(x=x3, y=y3, marker=dict(color='lightgreen'), orientation ='h')])
-fig3.update_layout(width=800, height=800, title=f'Most popular topics for Game {sel_id}', xaxis_title='Topic', yaxis_title='Frequency',
-                                    title_font=dict(
-                                    family="Arial",
-                                    size=24,
-                                    color="black"
-                                    ))
-
-@st.cache_data(ttl=10000)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-text_per_id = run_query(f"""SELECT * FROM `whizbang.whizbang_dataset.text_per_id_1000`
-                        ORDER BY _char_count DESC
-                        LIMIT 20""")
-
-text_per_id = pd.DataFrame(text_per_id)
-text = text_per_id[text_per_id['_id'] == sel_id]['_clean_text'].values[0]
-stopwords = stopwords.words('english')
-context_stopwords = ['still','get', 'dont','cant','game','would', 'games', 'play', 'playing', 'played', 'player', 'players', 'playable']
-stopwords.extend(context_stopwords)
-
-wordcloud = WordCloud(background_color="white",
-                      max_words=50,
-                      width=800,
-                      height=600,
-                      min_font_size=6,
-                      collocations=False,
-                      stopwords = stopwords
-                      ).generate(text)
+#fig = go.Figure(data=[go.Bar(x=x1, y=y1, marker=dict(color='lightgreen'), orientation ='h')])
+fig = go.Figure()
 
 
-plt.figure(figsize=(8, 6))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
+fig.add_trace(go.Bar(x=x1, y=y1, marker=dict(color='lightgreen'), orientation ='h'))
+fig.update_layout(width=800, height=900, xaxis_title='Frequency', yaxis_title='Topic', title=None, margin=dict(t=0))
+
+
+
+
+
+
+
+# stopwords = stopwords.words('english')
+# context_stopwords = ['still','get', 'dont','cant','game','would', 'games', 'play', 'playing', 'played', 'player', 'players', 'playable']
+# stopwords.extend(context_stopwords)
+
+# wordcloud = WordCloud(background_color="white",
+#                       max_words=50,
+#                       width=800,
+#                       height=600,
+#                       min_font_size=6,
+#                       collocations=False,
+#                       stopwords = stopwords
+#                       ).generate(text)
+
+
+# plt.figure(figsize=(8, 6))
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis('off')
 
 #Reviews display
 
+# reviews = pd.read_csv('raw_data/reviews_topics_1000.csv')
+# reviews = reviews.rename(columns=lambda x: x.replace('_', ''))
+
 def display_reviews(topic, sel_id):
 
-    per_topic = reviews[reviews[topic] == 1].sort_values(by = 'votesup', ascending = False)
-    per_id_per_topic = per_topic[per_topic['id'] == sel_id].head(10)
-    top_reviews  = per_id_per_topic['review']
-    return top_reviews.iloc[0]
+    per_topic = reviews[(reviews[topic] == 1) & (reviews['id'] == sel_id) & (reviews['charcount'] < 200)].sort_values(by = 'votesup', ascending = False)
+    top_reviews  = per_topic['review']
+    return top_reviews
 
-# Call the function to create the bar charts
 
-col2, col3 = st.columns([1, 1 ])
-#with col1:
-#    with st.container():
-#        st.plotly_chart(fig1, use_container_width=True)
-#    with st.container():
-#        st.plotly_chart(fig2, use_container_width=True)
-with col2:
-    st.plotly_chart(fig3, use_container_width=False)
+col1, col2 = st.columns([1, 1 ])
+
+with col1:
+    st.subheader('Frequency of topics in reviews')
+    selected = plotly_events(
+    fig,
+    click_event=True,
+            )
     st.write('<p style="font-size:24px;"><b>Wordcloud of game reviews</b></p>',unsafe_allow_html=True)
-    st.pyplot()
-with col3:
-    st.write('<p style="font-size:24px;"><b>Uncover most popular reviews per topic</b></p>',unsafe_allow_html=True)
-    if st.button('AI'):
-        topic = 'ai'
-        review = display_reviews(topic, sel_id)
-        st.code(f'{review}')
+
+with col2:
+
+    with st.container():
+
+    #with st.container( style = {'max-height': '900px', 'overflow-y': 'scroll'}):
+        if selected:
+            st.subheader('Most popular reviews per topic')
+            topic = selected[0]['y']
+            reviews = game_info['top_reviews_per_topic'][f't_{topic}']
+            all_reviews = []
+            for review in reviews:
+                #all_reviews.append(f'{review} \n --- \n')
+                st.write(review)
+                st.write('---')
+
+    # st.write('<p style="font-size:24px;"><b>Uncover most popular reviews per topic</b></p>',unsafe_allow_html=True)
+    # if st.button('AI'):
+    #     topic = 'ai'
+    #     for i in range(5):
+    #         review = display_reviews(topic, sel_id)
+    #         #st.write(review.iloc[i])
+    #         words = review.iloc[i].split()
+    #         annotated_words = []
+    #         for i, word in enumerate(words):
+    #             if i < len(words) - 1 and words[i].lower() == "ai":
+    #                 annotated_words.append((word, "*"))
+    #             else:
+    #                 annotated_words.append(f"{word} ")
+    #         annotated_text(*annotated_words)
+    #         st.write("---")
+
+
+#selected_points = plotly_events(fig, hover_event = True)
+
+#st.session_state.counter = 0
 
 
 
+#st.write(selected_points['x'])
+#print(selected_points)
 
-reviews = pd.read_csv('raw_data/reviews_topics_1000.csv')
+# string = "This is a review about ai yes"
+# words = string.split()  # split the string into a list of words
+# annotated_words = []
+# for i, word in enumerate(words):
+#     if i < len(words) - 1 and words[i] == "ai":
+#         annotated_words.append((word, "*"))
+#     else:
+#         annotated_words.append(f"{word} ")
 
-
-
-# col1, col2 = st.columns([1, 1])
-# with col1:
-#     st.plotly_chart(fig2, use_container_width=True)
-# with col2:
-#     st.pyplot()
-
-
-st.write("Look up a raw dataframe for comparison")
-st.dataframe(df_game, use_container_width=True)
-
-
-
-
-#Vertical barchart
-
-#fig = px.bar(new_df, x='Game', y='value',
-#             height=300, log_y=logy, text_auto=textauto,
-#             title=title)
-
-#with st.expander('**Game Info**', expanded=True):
-#    st.plotly_chart(fig, use_container_width=True)
+# with st.echo():
+#     annotated_text(*annotated_words)
